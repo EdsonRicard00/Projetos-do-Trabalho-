744762ca7036f305c7e2821826e52ae21ba9a6b1
EdsonRicard00 <edrpj00@gmail.com>
2026-09-16 09:49:38 -0300

    Descrição da alteração

---
+++ Projetos Python/rodaGigante.py
@@ -0,0 +1,10 @@
+
+#Programa para Verificar se tem o tamanho ideal para Entrar na roda gigante. 
+
+
+
+roda = int(input("Digite sua altura em cm: Exemplo: 176 ou 180: "))
+if roda >=150:
+    print("você pode entrar na roda Gigante")
+else:
+    print("você não pode entrar na roda Gigante")
