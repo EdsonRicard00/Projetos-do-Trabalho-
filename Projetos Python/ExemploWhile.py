6bf57dad87c087468400d911b9b75953255c92b8
EdsonRicard00 <edrpj00@gmail.com>
2026-09-18 08:54:41 -0300

    treinamento exemplo while

---
+++ Projetos Python/ExemploWhile.py
@@ -0,0 +1,8 @@
+Vari=input("Digite S para somar  :")
+
+while Vari=="S" or Vari=="s":
+    num1=int(input("Digite o primeiro numero: "))
+    num2=int(input("Digite o segundo numero: "))
+    soma=num1+num2
+    print("A soma dos numeros é:",soma)
+    Vari=input("Digite S para somar  :") 
